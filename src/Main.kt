ea6b752dffd21216b2f116f83ab824f159ae1d57
william.188823 <williammbitencourtmesquita@gmail.com>
2026-03-04 21:48:44 -0300

    copied code from portal

--- src/Main.kt
+++ src/Main.kt
@@ -1,14 +1,27 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+    val listaDeRegras = listOf(
+        Requisito("Mínimo de 5 caracteres") { it.length >= 5 },
+        Requisito("Deve conter o ano do Hexa (2026)") { it.contains("2026") }
+    )
+    var senhaAprovada = false
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
-    }
+    do {
+        println("\nDigite sua senha:")
+        val entrada = readLine() ?: "" // Null Safety com operador Elvis
+        var erroEncontrado: String? = null
+        for (regra in listaDeRegras) {
+            if (!regra.validacao(entrada)) {
+                erroEncontrado = regra.mensagemErro
+                break
+            }
+        }
+        if (erroEncontrado != null) {
+            println("❌ ERRO: $erroEncontrado")
+        } else {
+            println("✅ SUCESSO! Senha aceite pelo Overlord.")
+            senhaAprovada = true
+        }
+    } while (!senhaAprovada)
 }
+
+class Requisito(val string: String, val function: () -> Boolean)
