ceb59d106a1a3ee98fa1c01df6864a53f9a5bf2b
william <williammbitencourtmesquita@gmail.com>
2026-03-08 18:11:58 -0300

    Finished exercise

--- src/Main.kt
+++ src/Main.kt
@@ -1,27 +1,78 @@
 fun main() {
+    fun containsEmoji(text: String): Boolean {
+        return text.codePoints().anyMatch { codePoint ->
+            Character.getType(codePoint).toByte() == Character.OTHER_SYMBOL
+        }
+    }
+
+    fun sumNumbersInString(text: String): Int {
+        var digits = ""
+        var sum = 0
+
+        for (char in text) {
+            if (char.isDigit()) {
+                digits += char
+                continue
+            }
+
+            if (digits.isNotEmpty() && digits != "-") {
+                sum += digits.toInt()
+            }
+
+            digits = if (char == '-') "-" else ""
+        }
+
+        if (digits.isNotEmpty() && digits != "-") {
+            sum += digits.toInt()
+        }
+
+        return sum
+    }
+
+    fun hasCapitalLetter(text: String): Boolean {
+        for (char in text) {
+            if (char.isUpperCase()) {
+                return true
+            }
+        }
+        return false
+    }
+
     val listaDeRegras = listOf(
         Requisito("Mínimo de 5 caracteres") { it.length >= 5 },
-        Requisito("Deve conter o ano do Hexa (2026)") { it.contains("2026") }
+        Requisito("Deve conter o ano do Hexa (2026)") { it.contains("2026") },
+        Requisito("Deve conter pelo menos uma letra maiúscula e um número") { it.contains("[0-9]".toRegex()) or hasCapitalLetter(it) },
+        Requisito("Deve conter a palavra 'shell' (sem distinguir maiúsculas/minúsculas).") { it.lowercase().contains("shell") },
+        Requisito("Deve conter pelo menos um Emoji (ex: ❄).") { containsEmoji(it) },
+        Requisito("Os valores somandos devem ser menores que 2026.") { sumNumbersInString(it) != 2026 }
     )
-    var senhaAprovada = false
 
+    var senhaAprovada = false
     do {
         println("\nDigite sua senha:")
-        val entrada = readLine() ?: "" // Null Safety com operador Elvis
+        val entrada = readLine() ?: ""
+
         var erroEncontrado: String? = null
+
         for (regra in listaDeRegras) {
             if (!regra.validacao(entrada)) {
                 erroEncontrado = regra.mensagemErro
                 break
             }
         }
+
         if (erroEncontrado != null) {
             println("❌ ERRO: $erroEncontrado")
         } else {
-            println("✅ SUCESSO! Senha aceite pelo Overlord.")
+            println("✅ SUCESSO! Senha aceita pelo Overlord.")
             senhaAprovada = true
         }
+
     } while (!senhaAprovada)
+
 }
 
-class Requisito(val string: String, val function: () -> Boolean)
+class Requisito(
+    val mensagemErro: String,
+    val validacao: (String) -> Boolean
+)
